63ac3835ddbec3d5b84961b3923efae30d5969e0
Aleksey <Aleksey@PC>
2024-02-21 02:29:10 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using ImGuiNET;
+using ClickableTransparentOverlay;
+// using Swed32;
+
+
+namespace SimpleHackStarter
+{
+    public class Program : Overlay
+    {
+        bool unlimitedMoney = false;
+        // IntPtr moduleBase;
+        // int moneyAddress;
+        // Swed swed = new Swed("processName");
+
+        protected override void Render()
+        {
+            ImGui.Begin("SimpleHack");
+            ImGui.Checkbox("unlimited money", ref unlimitedMoney);
+            ImGui.End();
+        }
+
+        public void Hack()
+        {
+            // moduleBase = swed.GetModuleBase(".exe");
+
+            while (true)
+            {
+                if (unlimitedMoney)
+                {
+                    
+                }
+                else
+                {
+                    
+                }
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Starting GUI...");
+            Program program = new Program();
+            program.Start().Wait();
+            Thread hackThread = new Thread(program.Hack) { IsBackground = true };
+            hackThread.Start();
+        }
+    }
+}
